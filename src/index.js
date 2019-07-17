d5621ef21c68a2923e22eb442d9bc1d98597275d
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-07-18 04:08:53 +1200

    Place clickable content into overlay of search result.  Send broadcast message to listeners on click.

--- src/index.js
+++ src/index.js
@@ -34,20 +34,128 @@ exports.FDI_KB_Query_Module = function(parent_in)  {
     };
   }
 
-  const renderResult = (result_parent, data) => {
-    let element = this.htmlToElement(require("./snippets/searchresult.html"))
-    let heading = element.querySelector("#mapcore_search_result_heading");
-    heading.innerHTML = data['Dataset Title']
-    let paragraph = element.querySelector("#mapcore_search_result_paragraph");
-    paragraph.innerHTML = data['Description']
-    let image_paragraph = element.querySelector("#mapcore_search_result_image_paragraph");
-    image_paragraph.classList.add("float-left")
+const renderTitle = (title, overlay) => {
+var return_title = undefined
+var title_text = title.substring(0, title.indexOf("<a"))
+title_text = title_text.replace(/_/g, " ")
+
+var link_text = title.substring(title.indexOf("<a"))
+var actual_title = link_text.replace("Link", title_text)
+if (overlay == undefined) {
+  return_title = title_text
+} else {
+  return_title = actual_title
+}
+
+return return_title
+}
+
+const renderDescription = (description, max_length) => {
+if (max_length == undefined) {
+  max_length = 100
+}
+
+  var trimmed_description = description.length < max_length ?
+          description :
+          description.substring(0, max_length - 3) + "..."
+
+   return trimmed_description
+}
+
+  const renderOverlayResult = (overlay_parent, data) => {
+    let element = this.htmlToElement(require("./snippets/searchresultoverlay.html"))
+    let heading = element.querySelector("#mapcore_search_result_heading")
+    heading.innerHTML = renderTitle(data['Dataset Title'], true)
+    let paragraph = element.querySelector("#mapcore_search_result_paragraph")
+    paragraph.innerHTML = renderDescription(data['Description'], 300)
+    if (isCuratedData(data)) {
+      let curated_paragraph = element.querySelector("#mapcore_search_result_curated_data_paragraph")
+      curated_paragraph.classList.add("hidden")
+    }
+    if (haveScaffold(data)) {
+      let target_element = element.querySelector("#mapcore_search_result_scaffold_map")
+      target_element.classList.remove("disabled-map")
+      target_element.classList.add("cursor-pointer")
+      target_element.onclick = function(event) {
+        let supplementary_data = {'species': data["Scaffold"]["species"], 'organ': data["Scaffold"]["organ"], 'annotation': data["Scaffold"]["annotation"]}
+        let message_data = {action: "scaffold-show", resource: data["DataViewer"]["uri"], data: supplementary_data, sender: 'query-engine'};
+        channel.postMessage(message_data)
+      }
+    }
+    if (haveDataViewer(data)) {
+      let target_element = element.querySelector("#mapcore_search_result_data_viewer_map")
+      target_element.classList.remove("disabled-map")
+      target_element.classList.add("cursor-pointer")
+      target_element.onclick = function(event) {
+        let supplementary_data = {'species': data["DataViewer"]["species"], 'organ': data["DataViewer"]["organ"], 'annotation': data["DataViewer"]["annotation"]}
+        let message_data = {action: "scaffold-show", resource: data["DataViewer"]["uri"], data: supplementary_data, sender: 'query-engine'};
+        channel.postMessage(message_data)
+      }
+    }
+    let image_block = element.querySelector("#mapcore_search_result_image")
+    image_block.classList.add("float-left")
     let img_id = data['Example Image']
     if (img_id) {
       let image = element.querySelector("#mapcore_search_result_thumbnail")
-      image.classList.remove('optional')
+      image_block.classList.remove('hidden')
       biolucidaclient.get_thumbnail(image, img_id)
     }
+
+    overlay_parent.appendChild(element)
+  }
+
+  const isCuratedData = (data) => {
+    let curated = false;
+    if ("Dataset Title" in data && data["Dataset Title"].includes("blackfynn")) {
+      curated = true;
+    }
+    return curated
+  }
+
+  const haveScaffold = (data) => {
+    let have = false;
+    if ("Scaffold" in data && "uri" in data["Scaffold"] && data["Scaffold"]["uri"]) {
+      have = true;
+    }
+    return have;
+  }
+
+  const haveDataViewer = (data) => {
+    let have = false;
+    if ("DataViewer" in data && "uri" in data["DataViewer"] && data["DataViewer"]["uri"]) {
+      have = true;
+    }
+    return have;
+  }
+
+  const renderResult = (result_parent, data) => {
+    let element = this.htmlToElement(require("./snippets/searchresult.html"))
+    let heading = element.querySelector("#mapcore_search_result_heading")
+    heading.innerHTML = renderTitle(data['Dataset Title'])
+    let paragraph = element.querySelector("#mapcore_search_result_paragraph")
+    paragraph.innerHTML = renderDescription(data['Description'])
+    if (isCuratedData(data)) {
+      let target_element = element.querySelector("#mapcore_search_result_curated_data_paragraph")
+      target_element.classList.add("hidden")
+    }
+    if (haveScaffold(data)) {
+      let target_element = element.querySelector("#mapcore_search_result_scaffold_map")
+      target_element.classList.remove("disabled-map")
+    }
+    if (haveDataViewer(data)) {
+      let target_element = element.querySelector("#mapcore_search_result_data_viewer_map")
+      target_element.classList.remove("disabled-map")
+    }
+    let image_block = element.querySelector("#mapcore_search_result_image")
+    image_block.classList.add("float-left")
+    let img_id = data['Example Image']
+    if (img_id) {
+      let image = element.querySelector("#mapcore_search_result_thumbnail")
+      image_block.classList.remove('hidden')
+      biolucidaclient.get_thumbnail(image, img_id)
+    }
+    let overlay_element = element.querySelector('#search_result_overlay')
+    renderOverlayResult(overlay_element, data)
     result_parent.appendChild(element)
   }
 
@@ -120,19 +228,43 @@ exports.FDI_KB_Query_Module = function(parent_in)  {
         if (blackfynn_id.includes('N:organization:618e8dd9-f8d2-4dc4-9abb-c6aaab2e78a0') &&
            blackfynn_id.includes('N:dataset:0170271a-8fac-4769-a8f5-2b9520291d03') ) {
            data[i]['Example Image'] = '106' //http://sparc.biolucida.net/link?l=vua1n9'
+           data[i]['Scaffold'] = {'uri': 'https://mapcore-bucket1.s3-us-west-2.amazonaws.com/ISAN/scaffold/use_case4/rat_heart_metadata.json', 'species': 'rat', 'organ': 'heart', 'annotation': 'UBERON:0000948'}
+           data[i]['DataViewer'] = {'uri': 'https://mapcore-bucket1.s3-us-west-2.amazonaws.com/ISAN/csv-data/use-case-4/RNA_Seq.csv', 'species': 'rat', 'organ': 'heart', 'annotation': 'UBERON:0000948'}
         }
       }
     }
     return data
   }
-  
+
+  const ObjToSource=(o)=> {
+    if (!o) return null;
+    let str="",na=0,k,p;
+    if (typeof(o) == "object") {
+        if (!ObjToSource.check) ObjToSource.check = new Array();
+        for (k=ObjToSource.check.length;na<k;na++) if (ObjToSource.check[na]==o) return '{}';
+        ObjToSource.check.push(o);
+    }
+    k="",na=typeof(o.length)=="undefined"?1:0;
+    for(p in o){
+        if (na) k = "'"+p+"':";
+        if (typeof o[p] == "string") str += k+"'"+o[p]+"',";
+        else if (typeof o[p] == "object") str += k+ObjToSource(o[p])+",";
+        else str += k+o[p]+",";
+    }
+    if (typeof(o) == "object") ObjToSource.check.pop();
+    if (na) return "{"+str.slice(0,-1)+"}";
+    else return "["+str.slice(0,-1)+"]";
+}
+
   const jsonifyResults = row => {
     if (row) {
 	  let array = [];
 	  for (let i = 0; i < row.length;i++) {
 	    let rowData = row[i].data;
 		array[i] = [];
+		obj = {}
 		for (let j = 0; j < rowData.length; j++) {
+		  obj[rowData[j].name] = rowData[j].value[0];
 		  array[i][rowData[j].name] = rowData[j].value[0];
 		}
 	  }
@@ -182,9 +314,9 @@ exports.FDI_KB_Query_Module = function(parent_in)  {
   }
 
   this.broadcastCallback = (message) => {
-    let search_input = parent.querySelector("#mapcore_search_input");
-    if (message.data.data.resource) {
-      search_input.value = message.data.data.resource
+    if (message.action === "query-data") {
+      let search_input = parent.querySelector("#mapcore_search_input");
+      search_input.value = message.resource
       doQuery()
     }
   }
@@ -208,14 +340,15 @@ exports.FDI_KB_Query_Module = function(parent_in)  {
   const initialise = () => {
     // Add my snippet for the query dialog to the parent element.
     biolucidaclient = new biolucidaclient_module();
-    channel = new BroadcastChannel('sparc-portal');
-    channel.addEventListener('message', this.broadcastCallback);
+    //channel = new BroadcastChannel('sparc-mapcore-channel');
+    channel = new (require('broadcast-channel').default)('sparc-mapcore-channel');
+    channel.onmessage = this.broadcastCallback //addEventListener('message', this.broadcastCallback);
     let container = parent.querySelector("#maptab_contents");
-    if (container !== undefined) {
+    if (container != undefined) {
       setupSearchResults(container);
     }
     container = parent.querySelector("#maptab_tabbar");
-    if (container !== undefined) {
+    if (container != undefined) {
       setupSearchWidget(container);
     }
   }
